07fde6ad5cb8ed6cfdf35089d108919085f0a514
Anastasiya <schwarzer300708held@gmail.com>
2020-10-06 18:21:47 +0300

    feat: add Simple-chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  
+  chainList: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
+    
     // remove line with error and write your code here
+    return this.chainList.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
+    
     // remove line with error and write your code here
+    (value !== undefined) ? this.chainList.push( `( ${value} )`) : this.chainList.push( '( )' );
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
+    
     // remove line with error and write your code here
+        if (!this.chainList[position - 1]) {
+          this.chainList.length = 0;
+          throw new Error('Error');
+    }
+
+    this.chainList.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
+    
     // remove line with error and write your code here
+    this.chainList.reverse();
+     return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
+    
     // remove line with error and write your code here
+    let copyChainList = this.chainList.slice();
+    this.chainList.length = 0;
+    return copyChainList.join('~~');
   }
 };
 
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,41 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
+module.exports = function transform( arr ) {
   // remove line with error and write your code here
+
+  if (!Array.isArray(arr)) {
+    throw new Error('Error');
+  }
+
+  let resultArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--discard-next') {
+      if (arr[i + 1] !== undefined) {
+        i += 1;
+      }
+    } else if (arr[i] === '--discard-prev') {
+      if (arr[i - 1] !== undefined &&
+          (arr[i - 1] === resultArr[resultArr.length - 1] ||
+           (isNaN( resultArr[resultArr.length - 1]) &&
+            isNaN( arr[i - 1] ) ) )) {
+        resultArr.pop();
+      }
+    } else if (arr[i] === '--double-next') {
+      if (arr[i + 1] !== undefined) {
+        resultArr.push(arr[i + 1], arr[i + 1]);
+        i += 1;
+      }
+    } else if (arr[i] === '--double-prev') {
+      if (arr[i - 1] !== undefined && 
+          (arr[i - 1] === resultArr[resultArr.length - 1] ||
+           (isNaN( resultArr[resultArr.length - 1] &&
+            isNaN(arr[i - 1])) ))) {
+        resultArr.push(arr[i - 1]);
+      }
+    } else {
+      resultArr.push(arr[i]);
+    }
+  }
+  return resultArr;
 };
