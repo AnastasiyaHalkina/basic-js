552c3d33ed5c927d8706d90efb850a0aa21faed9
Anastasiya <schwarzer300708held@gmail.com>
2020-10-30 10:11:55 +0300

    feat: add rest tasks

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
+
+  constructor() {
+    this.counter = 0;
+    this.maximum = 0;
+  }
+
+  calculateDepth( arr ) {
     // remove line with error and write your code here
+    if (Array.isArray(arr)) {
+      this.counter += 1;
+      this.maximum = (this.maximum > this.counter) ? this.maximum : this.counter;
+      
+      for (let el of arr) {
+        if (Array.isArray(el)) {
+                    
+          return this.calculateDepth(el);
+        } else {
+          continue;
+        }
+      }
+    }
+    return this.maximum;
   }
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,53 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(direct=true) {
+    this.direct = direct;
+  }
+
+  encrypt(message, key) {
+    if (message === undefined ||
+        key === undefined) {
+    throw new Error('Error');
+    }
+
+    let resultStr = '';
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let repeatKey = key.repeat( Math.ceil( message.length / key.length ) );
+    repeatKey = repeatKey.slice(0, message.length);
+
+    for (let i = 0; i < message.length; i++) {
+      let letterPosition = message[i].charCodeAt();
+
+        if (letterPosition >= 65 && letterPosition <= 90) {
+          let cipherLetter;
+          let keyPosition = repeatKey[i].charCodeAt();
+
+          let newLetterPosition = letterPosition - 65 + keyPosition;
+          if (newLetterPosition > 90) {
+            cipherLetter = String.fromCharCode( (newLetterPosition) - 26);
+          } else {
+            cipherLetter = String.fromCharCode(newLetterPosition);
+          }
+          
+          resultStr += cipherLetter;
+        } else {
+          resultStr += message[i];
+        }
+    }
+    return resultStr;
   }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  decrypt(message, key) {
+    if (message === undefined ||
+      key === undefined) {
+    throw new Error('Error');
+    }
+
   }
 }
-
 module.exports = VigenereCipheringMachine;
